b077b925ed07816f2470ff5feacb5a40dad8a7e7
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2025-05-12 14:34:46 +0000

    feat: implement PhotoQueryHandler to return user's active photo URLs

--- src/main/java/com/photoblog/photos/PhotoQueryHandler.java
+++ src/main/java/com/photoblog/photos/PhotoQueryHandler.java
@@ -5,11 +5,15 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.photoblog.models.Photo;
 import com.photoblog.utils.AuthorizerClaims;
+import com.photoblog.utils.DynamoDBUtil;
 import com.photoblog.utils.HeadersUtil;
 import com.photoblog.utils.S3Util;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PhotoQueryHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -29,7 +33,32 @@ public class PhotoQueryHandler implements RequestHandler<APIGatewayProxyRequestE
 
             }
 
+            List<Photo> photos = DynamoDBUtil.getActivePhotosByUserId(userId);
+            if(photos.isEmpty()){
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(200)
+                        .withHeaders(HeadersUtil.getHeaders())
+                        .withBody("{\"message\": \"No active photos found\"}");
+            }
 
+            List<String> photoUrls = photos.stream()
+                    .map(photo -> s3Util.getImage(userId, photo.getPhotoId()))
+                    .toList();
+
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withHeaders(HeadersUtil.getHeaders())
+                    .withBody(objectMapper.writeValueAsString(photoUrls));
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(401)
+                    .withHeaders(HeadersUtil.getHeaders())
+                    .withBody("{\"error\": \"Unauthorized: " + e.getMessage() + "\"}");
+        } catch (Exception e){
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(500)
+                    .withHeaders(HeadersUtil.getHeaders())
+                    .withBody("{\"error\": \"Failed to retrieve photos: " + e.getMessage() + "\"}");
         }
     }
 }
