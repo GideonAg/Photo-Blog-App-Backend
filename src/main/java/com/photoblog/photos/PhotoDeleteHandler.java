cde87f1f2f4c2316bd358d877d843194126d7b2e
kingsley <kingsleybotchwayedu11@gmail.com>
2025-05-12 03:44:28 -0500

    feat: authorizer claims class

--- src/main/java/com/photoblog/photos/PhotoDeleteHandler.java
+++ src/main/java/com/photoblog/photos/PhotoDeleteHandler.java
@@ -1,4 +1,15 @@
 package com.photoblog.photos;
 
+import java.util.HashMap;
+
+import com.amazonaws.event.DeliveryMode;
+import com.photoblog.utils.AuthorizerClaims;
+
 public class PhotoDeleteHandler {
+    try {
+        HashMap<String, String> claims = AuthorizerClaims.extractCognitoClaims((request))
+        String userId = claims.get("email");
+    } catch(err) {
+        System.out.println(err);
+    }
 }
