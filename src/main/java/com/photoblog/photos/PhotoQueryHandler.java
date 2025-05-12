8da2c6d2823bfb9e4d7b7fd423387459243fa84d
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2025-05-12 14:00:08 +0000

    feat: add PhotQueryHandler with initial userId extraction

--- src/main/java/com/photoblog/photos/PhotoQueryHandler.java
+++ src/main/java/com/photoblog/photos/PhotoQueryHandler.java
@@ -1,4 +1,35 @@
 package com.photoblog.photos;
 
-public class PhotoQueryHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.photoblog.utils.AuthorizerClaims;
+import com.photoblog.utils.HeadersUtil;
+import com.photoblog.utils.S3Util;
+
+import java.util.Map;
+
+public class PhotoQueryHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final S3Util s3Util = new S3Util();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context){
+        try {
+            Map<String, String> claims = AuthorizerClaims.extractCognitoClaims(input);
+            String userId = claims.get("userId");
+            if(userId == null){
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(401)
+                        .withHeaders(HeadersUtil.getHeaders())
+                        .withBody("{\"error\": \"Unauthorized: Missing userId\"}");
+
+            }
+
+
+        }
+    }
 }
