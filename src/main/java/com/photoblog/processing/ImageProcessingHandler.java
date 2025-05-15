e5f0afe314aac003e41ddf91552063d8d461a354
Julius <juliussowah@amalitech.com>
2025-05-15 08:17:05 +0000

    refactor: Logged after the image is uploaded

--- src/main/java/com/photoblog/processing/ImageProcessingHandler.java
+++ src/main/java/com/photoblog/processing/ImageProcessingHandler.java
@@ -74,17 +74,15 @@ public class ImageProcessingHandler implements RequestHandler<SQSEvent, String> 
 
             File unprocessedFile = downloadFileFromS3(sourceKey, context);
             File processedFile = processImage(unprocessedFile, WATERMARK_TEXT, context);
-<<<<<<< HEAD
+
             ImageMetadataDTO imageMetadata = uploadProcessedFile(sourceKey, processedFile, context);
-=======
-            uploadProcessedFile(sourceKey, processedFile, context);
->>>>>>> 12c8270dba86fa9b374f4dab630d3a0baa09d858
 
 
             context.getLogger().log("Image processing completed for: " + sourceKey);
             sesUtil.sendProcessingCompletedEmail(userEmail, sourceKey);
 
             savePhotoToDynamoDB("userId",sourceKey, imageMetadata.getObjectUrl(), imageMetadata.getVersionId() );
+            context.getLogger().log("Image metadata saved to DynamoDB for: " + sourceKey);
 
             context.getLogger().log("Deleting unprocessed file: " + sourceKey);
             deleteUnprocessedFile(sourceKey, context);
